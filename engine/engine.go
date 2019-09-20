8dbea33c4de883d12fd1932be0552249c89e309a
肖泽锋 <qq523107430@163.com>
2019-09-20 08:19:17 +0800

    修改engine

--- engine/engine.go
+++ engine/engine.go
@@ -15,16 +15,24 @@ func Run(seeds ...Request) {
 		var curRequest = requests[0]
 		requests = requests [1:]
 
-		log.Printf("Fetching url:%s", curRequest.Url)
-		content, err := fetcher.Fetch(curRequest.Url)
+		parseResult, err := worker(curRequest)
 		if err != nil {
-			log.Printf("fetch url: %s, error: %v", curRequest.Url, err)
 			continue
 		}
-		parseResult := curRequest.ParseFunc(content)
+
 		requests = append(requests, parseResult.Requests...)
 		for _, item := range parseResult.Items {
 			log.Printf("Got item:%#v", item)
 		}
 	}
+}
+
+func worker(r Request) (ParseResult, error) {
+	log.Printf("Fetching url:%s", r.Url)
+	content, err := fetcher.Fetch(r.Url)
+	if err != nil {
+		log.Printf("fetch url: %s, error: %v", r.Url, err)
+		return ParseResult{}, err
+	}
+	return r.ParseFunc(content), nil
 }
